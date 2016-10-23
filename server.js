4e85cef720f66cc0e796bf0d2182b92612478dac
B130382EC <muthukuru_b130382ec@nitc.ac.in>
2016-10-23 17:18:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ var articleOne = {
     heading:'Article One',
     date:'Oct 23,2016',
     content: `<p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                This is the content for my first article.This is the content for my first article.
               </p>
               `
 };
