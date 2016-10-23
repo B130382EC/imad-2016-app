0dae05d172b61e344a9d6f0825645f100d223439
B130382EC <muthukuru_b130382ec@nitc.ac.in>
2016-10-23 20:10:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,13 +8,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 
 //move the image
-var image = document.getElementById('madi');
-var marginLeft=0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    image.style.marginLeft=marginLeft+'px';
-}
-
-image.onclick = function () {
-    var interval=setInterval(moveRight,50);
+var img = document.getElementById('madi');
+img.onclick=function(){
+  img.style.marginLeft='100px';  
 };
