ba184a8d62f5607837219592c0d45f3326a8ad6a
B130382EC <muthukuru_b130382ec@nitc.ac.in>
2016-10-23 20:07:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool=require('pg').Pool;
 
-var config={
-    user:'b130382ec',
-    database:'b130382ec',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB-PASSWORD
-};
 
 var app = express();
 app.use(morgan('combined'));
@@ -85,25 +77,6 @@ var htmlTemplate=`
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var counter=0;
-app.get('/counter',function(req,res){
-   counter=counter+1;
-   res.send(counter.toString());
-});
-var pool=new Pool(config);
-app.get('/test-db',function(req,res){
-   //make a select request 
-   //and return a response with the results
-   pool.query('SELECT * FROM test',function(err,result){
-       if(err){
-           res.status(500).send(err.toString());
-       }
-       else{
-           res.send(JSON.stringify(result));    
-           }
-   }); 
 });
 
 app.get('/:articleName',function(req,res){
