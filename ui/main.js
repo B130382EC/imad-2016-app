6fdc7f33dc1dd28d19287837e1fff57c5b0b9339
B130382EC <muthukuru_b130382ec@nitc.ac.in>
2016-11-18 18:15:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,43 +1,21 @@
-// Counter code
+console.log('Loaded!');
 
-var button = document.getElementById('counter');
-button.onclick= function(){
- 
- //Make a request to the counter end point
- var request= new XMLHttpRequest();
- 
- //capture the response and store it in a variable
- 
- request.onreadystatechange=function(){
-     if(request.readyState==XMLHttpRequest.DONE){
-         //Take some action
-         if(request.status==200) {
-             var counter=request.responseText;
-             var span= document.getElementById('count');
-             span.innerHTML=counter.toString();
-         }
-     }
-     //not yet done
- };
- 
- 
- //make the request 
- request.open('GET','http://b130382ec.imad.hasura-app.io/counter',true);
- request.send(null);
+//change the text of the main-text div
+
+var element=document.getElementById('main-text');
+
+
+element.innerHTML='New Value';
+
+//move the image
+var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight () {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft=marginLeft+'px';
+}
+
+img.onclick = function () {
+    var interval=setInterval(moveRight,50);
 };
 
-//submit name
-var nameInput=document.getElementById('name');
-var name = nameInput.value;
-var submit=document.getElementById('submit_btn');
-submit.onclick=function() {
-  // Make a request to the server and send the name
-  
-  //capture a list of names and render it as a list
-  var names=['name1','name2','name3'];
-  var list='';
-  for(var i=0;i<names.length;i++){
-      list<='<li>'++names[i]+ '<li>';
-  }
-  var 
-};
